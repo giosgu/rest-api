bb69e94dda8cda19ef040f457a6a03fc6951d093
GIOSA GUIDO <MT30886835@osdebinario.grupoosde>
2019-01-25 18:34:45 -0300

    .

--- back/src/controllers/casoController.ts
+++ back/src/controllers/casoController.ts
@@ -9,20 +9,9 @@ export class CasoController{
 
     public get(req: Request, res: Response) {
         console.log("/Casos : Get");
-/*        
-        let direccion1 : Direccion = new Direccion("321321321", "32321321", "Bulnes", "3727")
-        let caso1 : Caso = new Caso("Dolor de cabeza", "61205254", direccion1);
-
-        let direccion2 : Direccion = new Direccion("321321321", "32321321", "Córdoba", "3727")
-        let caso2 : Caso = new Caso("Dolor de cabeza", "6012458721", direccion2);
-
-        let casos : Array<Caso> = new Array; 
-        casos.push(caso1)
-        casos.push(caso2)
-        let result: Result = new Result(casos);
-*/      const path = require("path");
-        console.log(path.resolve("../back/src/data/users.json"));
-        var obj = JSON.parse(fs.readFileSync(path.resolve("../back/src/data/users.json"), 'utf8'));
+        const path = require("path");
+        console.log(path.resolve("../back/src/data/casos.json"));
+        var obj = JSON.parse(fs.readFileSync(path.resolve("../back/src/data/casos.json"), 'utf8'));
         res.setHeader('Content-Type', 'application/json');
         res.status(200).send(JSON.stringify(obj));
         
